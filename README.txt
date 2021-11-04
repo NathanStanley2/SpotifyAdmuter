70bb8349c7fc25022393dec60434271cad17dc95
NathanStanley2 <56930423+NathanStanley2@users.noreply.github.com>
2021-11-04 10:27:04 -0500

    first commit

---
+++ README.txt
@@ -0,0 +1,6 @@
+Youtube Ad Muter
+The program when launched will open a Youtube Tab. 
+When a video is played and an Ad pops up your computer
+will be muted. 
+After the ad is gone it will unmute your computer. 
+The launch file is screengrab.py. 
---
+++ muter.py
@@ -0,0 +1,24 @@
+#credit to https://github.com/AndreMiras/pycaw
+from ctypes import cast, POINTER
+from comtypes import CLSCTX_ALL
+from pycaw.pycaw import AudioUtilities, IAudioEndpointVolume
+import time
+
+devices = AudioUtilities.GetSpeakers()
+interface = devices.Activate(IAudioEndpointVolume._iid_, CLSCTX_ALL, None)
+volume = cast(interface, POINTER(IAudioEndpointVolume))
+#DO NOT LAUNCH THIS FILE TO STARE
+
+#x = input("Enter a letter to mute the thing ")
+
+def volume_muter():
+    #if x == ("a"):
+    volume.SetMasterVolumeLevel(-65.25, None) # range is -65.25 to 0, -65 is lowest, 0 is max volume
+
+def volume_unmuter():
+    volume.SetMasterVolumeLevel(-5, None) #unmute and set volume to 72
+    #else:
+       # print("not muted")
+
+#mutes the computer with the library
+
